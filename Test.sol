53a809cf5d016414844a0aab550fcfe0fb42c479
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-02-14 16:56:18 -0500

    Update Test.sol

--- Test.sol
+++ Test.sol
@@ -3,14 +3,13 @@ pragma solidity ^0.4.25;
 import "./Public_external.sol"
 
 contract Test {
+    uint[20] a;
     
     function test_public() {
-        uint[20] a;
         new Public_external().pub(a);
     }
     
     function test_external() {
-        uint[20] a;
         new Public_external().ext(a);
     }
 }
